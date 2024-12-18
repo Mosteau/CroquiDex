711508d24ed46156502783cd03c94e9833337a3b
BahAilime <emilia.beguin@gmail.com>
2024-12-18 16:07:05 +0100

    Ajout du pokemon précédent et suivant dans la vue detail

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .pokeapi import get_pokemon_by_id, get_pokemon_by_name
+from .pokemon import Pokemon
 
 # Create your views here.
 def HomeListPokemon(request):
@@ -12,8 +13,13 @@ def EquipePokemon(request):
     return render(request, 'equipe.html')
 
 def DetailPokemon(request, name):
-    pokemon = get_pokemon_by_name(name)
-    return render(request, 'detail.html', {'pokemon': pokemon})
+    pokemonJson = get_pokemon_by_name(name)
+    poke = Pokemon(pokemonJson)
+    poke.id
+    pokeBefore = get_pokemon_by_id(poke.id + 1 if poke.id > 0 else 0)
+    pokeAfter = get_pokemon_by_id((poke.id + 1)%151)
+    
+    return render(request, 'detail.html', {'pokemon': pokemonJson, 'before': pokeBefore, 'after': pokeAfter})
 
 def FightClubPokemon(request):
     return render(request, 'fightclub.html')
