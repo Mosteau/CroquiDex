58a6f8681f759b46969e14da37c84c8d7ca7be81
BahAilime <emilia.beguin@gmail.com>
2024-12-18 16:06:21 +0100

    Ajout de la classe Pokemon

---
+++ main/pokemon.py
@@ -0,0 +1,25 @@
+class Pokemon:
+    def __init__(self, data):
+        self.id = data.get("id")
+        self.name = data.get("name")
+
+        self.types = [t["type"]["name"] for t in data.get("types", [])]
+
+        self.abilities = [a["ability"]["name"] for a in data.get("abilities", [])]
+
+        self.stats = {stat["stat"]["name"]: stat["base_stat"] for stat in data.get("stats", [])}
+
+        self.base_experience = data.get("base_experience")
+
+        self.weight = data.get("weight")
+        self.height = data.get("height")
+
+        self.sprites = data.get("sprites", {}).get("front_default")
+
+    def __repr__(self):
+        return (
+            f"Pokemon(id={self.id}, name='{self.name}', types={self.types}, "
+            f"abilities={self.abilities}, stats={self.stats}, "
+            f"base_experience={self.base_experience}, "
+            f"weight={self.weight}, height={self.height})"
+        )
