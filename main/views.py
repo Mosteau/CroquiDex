6fa413217435b01aca505dfdc2c1394b4ad34a33
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-12-18 14:54:29 +0100

    creation page equipe

--- main/views.py
+++ main/views.py
@@ -8,11 +8,33 @@ def HomeListPokemon(request):
     
     return render(request, 'home.html', {'pokemon': str(test)})
 
+from django.shortcuts import render
+from .pokeapi import get_pokemon_by_id
+
+# Create your views here.
+def HomeListPokemon(request):
+    
+    test = get_pokemon_by_id(25)
+    
+    return render(request, 'home.html', {'pokemon': str(test)})
+
 def EquipePokemon(request):
-    return render(request, 'equipe.html')
+    # Récupérer les 151 premiers Pokémon
+    pokemons = []
+    for pokemon_id in range(1, 151):
+        pokemon = get_pokemon_by_id(pokemon_id)
+        if pokemon:
+            pokemons.append({
+                'id': pokemon['id'],
+                'name': pokemon['name'].capitalize(),
+                'image': pokemon['sprites']['front_default']
+            })
+    
+    return render(request, 'equipe.html', {'pokemons': pokemons})
 
 def DetailPokemon(request):
     return render(request, 'detail.html')
 
 def FightClubPokemon(request):
     return render(request, 'fightclub.html')
+
