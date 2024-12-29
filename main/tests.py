0e27eed2ec103234b7b6d02ea58b54eb37bb29a2
BahAilime <emilia.beguin@gmail.com>
2024-12-29 21:39:53 +0100

    Ajout des tests "get_all_pokemon" et class pokemon

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .pokeapi import get_pokemon_by_id, get_pokemon_by_name
+from .pokeapi import get_pokemon_by_id, get_pokemon_by_name, get_all_pokemon
+from .pokemon import Pokemon
 
 class PokeAPITest(TestCase):
 
@@ -24,3 +25,21 @@ class PokeAPITest(TestCase):
         """get_pokemon_by_id should return None if the pokemon does not exist"""
         pokemon = get_pokemon_by_id(0)
         self.assertIsNone(pokemon)
+
+    def test_get_all_pokemon(self):
+        """get_all_pokemon should return a list of all pokemon"""
+        pokemon = get_all_pokemon()
+        self.assertEqual(len(pokemon), 151)
+
+class PokemonTest(TestCase):
+    def test_pokemon(self):
+        pokemon = get_pokemon_by_id(1)
+        poke = Pokemon(pokemon)
+        self.assertEqual(poke.id, 1)
+        self.assertEqual(poke.name, 'bulbasaur')
+        self.assertEqual(poke.types, ['grass', 'poison'])
+        self.assertEqual(poke.abilities, ['overgrow', 'chlorophyll'])
+        self.assertEqual(poke.stats, {'hp': 45, 'attack': 49, 'defense': 49, 'special-attack': 65, 'special-defense': 65, 'speed': 45})
+        self.assertEqual(poke.base_experience, 64)
+        self.assertEqual(poke.weight, '6.9 kg')
+        self.assertEqual(poke.height, '70 cm')
